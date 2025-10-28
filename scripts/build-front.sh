a232e20db18e087396439f8aece8bbf043ee464a
belthlemar <martin.belthle@rte-france.com>
2025-10-28 13:57:34 +0100

    try to understand

--- scripts/build-front.sh
+++ scripts/build-front.sh
@@ -6,7 +6,6 @@ CURR_DIR=$(cd "$(dirname "$0")" && pwd)
 
 cd "$CURR_DIR"/../antares_web/webapp
 
-npm run build -- --mode=desktop
+npm run build --mode=desktop
 
-cd ..
-cp -r antares_web/webapp/dist/ resources/webapp
+cp -r ../antares_web/webapp/dist/ resources/webapp
