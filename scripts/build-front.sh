721f4e1283523beb928551bd5acdf91a62de7f49
belthlemar <martin.belthle@rte-france.com>
2025-10-28 14:08:38 +0100

    should wrok but doesnot

--- scripts/build-front.sh
+++ scripts/build-front.sh
@@ -6,6 +6,6 @@ CURR_DIR=$(cd "$(dirname "$0")" && pwd)
 
 cd "$CURR_DIR"/../antares_web/webapp
 
-npm run build --mode=desktop
+npm run build -- --mode=desktop
 
 cp -r ../antares_web/webapp/dist/ resources/webapp
