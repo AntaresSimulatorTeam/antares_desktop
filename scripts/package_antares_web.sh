1aa58e7718405157f8a359085d95d0de9508b168
belthlemar <martin.belthle@rte-france.com>
2025-10-28 17:32:07 +0100

    use whitespaces instead of tabs

--- scripts/package_antares_web.sh
+++ scripts/package_antares_web.sh
@@ -57,8 +57,7 @@ declare -A YAML_VERSION_MAP=(
   ["9.3.1"]="930"
 )
 
-SOLVER_MAPPING_IN_CONFIG_FILE=""
-TAB=$'\t'
+SOLVER_MAPPING_IN_CONFIG_FILE="\n"
 
 for KEY in "${!VERSION_MAP[@]}"; do
   LINK="https://github.com/AntaresSimulatorTeam/Antares_Simulator/releases/download/v$KEY/$ANTARES_SOLVER_ZIPFILE_NAME"
@@ -82,7 +81,7 @@ for KEY in "${!VERSION_MAP[@]}"; do
     tar xzf $ANTARES_SOLVER_ZIPFILE_NAME
     SOLVER_NAME="antares$SOLVER_PATH-solver"
   fi
-  SOLVER_MAPPING_IN_CONFIG_FILE+="${TAB}${TAB}${TAB}${TAB}$YAML_SOLVER_NAME: .\/AntaresWeb\/antares_solver\/$FOLDER_NAME\/$SOLVER_NAME\n"
+  SOLVER_MAPPING_IN_CONFIG_FILE+="        $YAML_SOLVER_NAME: .\/AntaresWeb\/antares_solver\/$FOLDER_NAME\/$SOLVER_NAME\n"
   rm $ANTARES_SOLVER_ZIPFILE_NAME
   cd ..
 done
