f230431e23417dd66cb94af4c71cee38d6a362a1
belthlemar <martin.belthle@rte-france.com>
2026-03-03 10:35:11 +0100

    inverse windows and linux

--- scripts/package_antares_web.sh
+++ scripts/package_antares_web.sh
@@ -31,12 +31,12 @@ rm -rf ${DIST_DIR}
 echo "INFO: Generating the Desktop version of the Web Application..."
 if [[ "$OSTYPE" == "msys"* ]]; then
   pushd ${PROJECT_DIR}
-  source .venv/bin/activate  # Enter the virtual env to use pyinstaller
+  .venv\Scripts\activate  # Enter the virtual env to use pyinstaller
   pyinstaller --distpath ${DIST_DIR} AntaresWebWin.spec
   popd
 else
   pushd ${PROJECT_DIR}
-  .venv\Scripts\activate  # Enter the virtual env to use pyinstaller
+  source .venv/bin/activate  # Enter the virtual env to use pyinstaller
   pyinstaller --distpath ${DIST_DIR} AntaresWebLinux.spec
   popd
 fi
