9f18b529d5043bea66f8e2331da510520a81f8b5
belthlemar <martin.belthle@rte-france.com>
2026-03-03 10:26:48 +0100

    use pyinstaller on the whole system

--- scripts/package_antares_web.sh
+++ scripts/package_antares_web.sh
@@ -31,11 +31,11 @@ rm -rf ${DIST_DIR}
 echo "INFO: Generating the Desktop version of the Web Application..."
 if [[ "$OSTYPE" == "msys"* ]]; then
   pushd ${PROJECT_DIR}
-  uv run pyinstaller --distpath ${DIST_DIR} AntaresWebWin.spec
+  pyinstaller --distpath ${DIST_DIR} AntaresWebWin.spec
   popd
 else
   pushd ${PROJECT_DIR}
-  uv run pyinstaller --distpath ${DIST_DIR} AntaresWebLinux.spec
+  pyinstaller --distpath ${DIST_DIR} AntaresWebLinux.spec
   popd
 fi
 
