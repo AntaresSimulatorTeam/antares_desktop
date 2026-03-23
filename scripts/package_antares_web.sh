a22f0b6bfcf3952b59093092ed293326b6baa606
belthlemar <martin.belthle@rte-france.com>
2026-03-23 13:19:59 +0100

    try to find

--- scripts/package_antares_web.sh
+++ scripts/package_antares_web.sh
@@ -31,12 +31,14 @@ rm -rf ${DIST_DIR}
 echo "INFO: Generating the Desktop version of the Web Application..."
 
 pushd ${PROJECT_DIR}
+cd antares_web
 if [[ "$OSTYPE" == "msys"* ]]; then
   uv run pyinstaller --distpath ${DIST_DIR} AntaresWebWin.spec
 else
   uv run pyinstaller --distpath ${DIST_DIR} AntaresWebLinux.spec
 fi
 popd
+cd ..
 
 echo "INFO: Creating destination directory '${ANTARES_SOLVER_DIR}'..."
 mkdir -p "${ANTARES_SOLVER_DIR}"
