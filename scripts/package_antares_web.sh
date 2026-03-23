9c166a3d5c627af4c8929195b02e2145ac557993
belthlemar <martin.belthle@rte-france.com>
2026-03-23 13:50:24 +0100

    f

--- scripts/package_antares_web.sh
+++ scripts/package_antares_web.sh
@@ -32,8 +32,7 @@ echo "INFO: Generating the Desktop version of the Web Application..."
 
 pushd ${PROJECT_DIR}
 if [[ "$OSTYPE" == "msys"* ]]; then
-  cd antares_web
-  .venv\Scripts\activate  # Enter the virtual env to use pyinstaller
+  source antares_web/.venv/bin/activate  # Enter the virtual env to use pyinstaller
   pyinstaller --distpath ${DIST_DIR} ../AntaresWebWin.spec
 else
   source antares_web/.venv/bin/activate  # Enter the virtual env to use pyinstaller
